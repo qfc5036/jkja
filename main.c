6abe642dadfaeadd6ed8c7ce6e88c19d619c23c1
qfc5036 <qfc5036@psu.edu>
2020-09-10 17:35:01 +0000

    l

--- main.c
+++ main.c
@@ -6,34 +6,34 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 int main(void) {
-  char * grade = readline("Enter your CMPSC131 grade: ");
+  char * grade = readline("Enter your CMPSC 131 grade: ");
   double grades = atof(grade);
   if (grades>=93.0){
-    puts ("Your letter grade for CMPSC131 is A.");
+    puts ("Your letter grade for CMPSC 131 is A.");
   }
   else if (grades>=90.0){
-    puts ("Your letter grade for CMPSC131 is A-.");
+    puts ("Your letter grade for CMPSC 131 is A-.");
   }
   else if (grades>=87.0){
-    puts ("Your letter grade for CMPSC131 is B+.");
+    puts ("Your letter grade for CMPSC 131 is B+.");
   }
   else if (grades>=83.0){
-    puts ("Your letter grade for CMPSC131 is B.");
+    puts ("Your letter grade for CMPSC 131 is B.");
   }
   else if (grades>=80.0){
-    puts ("Your letter grade for CMPSC131 is B-.");
+    puts ("Your letter grade for CMPSC 131 is B-.");
   }
   else if (grades>=77.0){
-    puts ("Your letter grade for CMPSC131 is C+.");
+    puts ("Your letter grade for CMPSC 131 is C+.");
   }
   else if (grades>=70.0){
-    puts ("Your letter grade for CMPSC131 is C.");
+    puts ("Your letter grade for CMPSC 131 is C.");
   }  
   else if (grades>=60.0){
-    puts ("Your letter grade for CMPSC131 is D.");
+    puts ("Your letter grade for CMPSC 131 is D.");
   }
   else {
-    puts ("Your letter grade for CMPSC131 is F.");
+    puts ("Your letter grade for CMPSC 131 is F.");
   }
   return 0;
 }
